f6e3b7643c27e126323bd6e877b1ead053425eb3
Ted Stacy <tedstacy2@gmail.com>
2018-09-07 23:49:11 -0400

    missed one file, added all files to Euchre/

--- Game.py
+++
@@ -1,9 +0,0 @@
-from Trick import Trick
-from random import randint
-
-class Game:
-	def __init__(self):
-		self.dealer = None
-
-	def determine_dealer(self, players):
-		self.dealer = players[randint(0, 4)]
